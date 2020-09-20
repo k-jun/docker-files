7fa18b3716a138e273edc1753a7a96449fff2543
keijun.kumagai <keijun091221@gmail.com>
2020-09-20 14:14:57 +0900

    feat(mysql): regular update

--- mysql/master-db/scripts/00_scheme.sql
+++ mysql/master-db/scripts/00_scheme.sql
@@ -35,3 +35,16 @@
 --     created_at INTEGER NOT NULL,
 --     updated_at INTEGER NOT NULL
 -- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE parent (
+    id INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+CREATE TABLE child (
+    id INT,
+    parent_id INT,
+    INDEX par_ind (parent_id),
+    FOREIGN KEY (parent_id) REFERENCES parent(id) ON DELETE CASCADE
+) ENGINE=INNODB;
+
--- mysql/slave-db/scripts/00_init.sh
+++ mysql/slave-db/scripts/00_init.sh
@@ -1,24 +1,24 @@
-#!/bin/sh
-
-# masterをロックする
-mysql -u root -h mysqld-master --password=dena -e "RESET MASTER;"
-mysql -u root -h mysqld-master --password=dena -e "FLUSH TABLES WITH READ LOCK;"
-
-# masterのDB情報をDumpする
-mysqldump -uroot -h mysqld-master --password=dena --all-databases --master-data --single-transaction --flush-logs --events > /tmp/master_dump.sql
-
-# dumpしたmasterのDBをslaveにimportする
-mysql -u root --password=dena -e "STOP SLAVE;"
-mysql -u root --password=dena < /tmp/master_dump.sql
-
-# masterに繋いで bin-logのファイル名とポジションを取得する
-log_file=`mysql -u root -h mysqld-master --password=dena -e "SHOW MASTER STATUS\G" | grep File: | awk '{print $2}'`
-pos=`mysql -u root -h mysqld-master --password=dena -e "SHOW MASTER STATUS\G" | grep Position: | awk '{print $2}'`
-
-# slaveの開始
-mysql -u root --password=dena -e "RESET SLAVE;"
-mysql -u root --password=dena -e "CHANGE MASTER TO MASTER_HOST='mysqld-master', MASTER_USER='root', MASTER_PASSWORD='dena', MASTER_LOG_FILE='${log_file}', MASTER_LOG_POS=${pos};"
-mysql -u root --password=dena -e "START SLAVE;"
-
-# masterをunlockする
-mysql -u root --password=dena -h mysqld-master -e "UNLOCK TABLES;"
+# #!/bin/sh
+#
+# # masterをロックする
+# mysql -u root -h mysqld-master --password=dena -e "RESET MASTER;"
+# mysql -u root -h mysqld-master --password=dena -e "FLUSH TABLES WITH READ LOCK;"
+#
+# # masterのDB情報をDumpする
+# mysqldump -uroot -h mysqld-master --password=dena --all-databases --master-data --single-transaction > /tmp/master_dump.sql
+#
+# # dumpしたmasterのDBをslaveにimportする
+# mysql -u root --password=dena -e "STOP SLAVE;"
+# mysql -u root --password=dena < /tmp/master_dump.sql
+#
+# # masterに繋いで bin-logのファイル名とポジションを取得する
+# log_file=`mysql -u root -h mysqld-master --password=dena -e "SHOW MASTER STATUS\G" | grep File: | awk '{print $2}'`
+# pos=`mysql -u root -h mysqld-master --password=dena -e "SHOW MASTER STATUS\G" | grep Position: | awk '{print $2}'`
+#
+# # slaveの開始
+# mysql -u root --password=dena -e "RESET SLAVE;"
+# mysql -u root --password=dena -e "CHANGE MASTER TO MASTER_HOST='mysqld-master', MASTER_USER='root', MASTER_PASSWORD='dena', MASTER_LOG_FILE='${log_file}', MASTER_LOG_POS=${pos};"
+# mysql -u root --password=dena -e "START SLAVE;"
+#
+# # masterをunlockする
+# mysql -u root --password=dena -h mysqld-master -e "UNLOCK TABLES;"
